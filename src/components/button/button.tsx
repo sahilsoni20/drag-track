90f8589a757ee6a4464ba5a68ea3201819a2bfc2
sahilsoni20 <ssahil3120@gmail.com>
2024-07-04 11:13:19 +0530

    🏁

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,14 +1,20 @@
-import {LucideIcon} from 'lucide-react'
+import { LucideIcon } from "lucide-react";
 
 type ButtonProps = {
-  onClick: (() => void) | undefined
-  label: string
-  fullWidth: boolean
-  bgLight: boolean
-  variant?: | "default" | "destructive" | "outline" | "secondary" | "ghost" | "link"
-  disabled?: boolean
-  icon?: LucideIcon
-}
+  onClick: (() => void) | undefined;
+  label: string;
+  fullWidth: boolean;
+  bgLight: boolean;
+  variant?:
+    | "default"
+    | "destructive"
+    | "outline"
+    | "secondary"
+    | "ghost"
+    | "link";
+  disabled?: boolean;
+  icon?: LucideIcon;
+};
 
 const Button = ({
   onClick,
@@ -17,12 +23,14 @@ const Button = ({
   bgLight = false,
   variant = "default",
   disabled = false,
-  icon: Icon
-
+  icon: Icon,
 }: ButtonProps) => {
-  const baseClass = "flex items-center justify-center gap-3 rounded md:rounded-md text-xs font-bold md:text-sm md:font-medium ring-offset-background transition-colors focus-visible:outline-none"
-  const fullWidthClass = fullWidth ? 'w-full text-center' : ''
-  const bgLightClass = bgLight ? "bg-indigo-100 hover:bg-indigo-200 text-indigo-600 hover:text-indigo-800" : ""
+  const baseClass =
+    "flex items-center justify-center gap-3 rounded md:rounded-md text-xs font-bold md:text-sm md:font-medium ring-offset-background transition-colors focus-visible:outline-none";
+  const fullWidthClass = fullWidth ? "w-full text-center" : "";
+  const bgLightClass = bgLight
+    ? "bg-indigo-100 hover:bg-indigo-200 text-indigo-600 hover:text-indigo-800"
+    : "";
   const variantClass = {
     default: "bg-indigo-500 text-white hover:bg-indigo-600",
     destructive: "bg-red-600 text-white hover:bg-red-700",
@@ -31,18 +39,20 @@ const Button = ({
     ghost:
       "bg-transparent text-slate-600 hover:bg-slate-200 hover:text-slate-800 font-semibold",
     link: "text-blue-600 underline hover:text-blue-800",
-  }
+  };
 
   return (
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`${baseClass} ${fullWidthClass} ${bgLightClass} ${!bgLight && variantClass[variant]}`}
+      className={`${baseClass} ${fullWidthClass} ${bgLightClass} ${
+        !bgLight && variantClass[variant]
+      }`}
     >
-      {Icon && <Icon size={18}/>}
+      {Icon && <Icon size={18} />}
       {label}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,9 +1,43 @@
+import { useEffect, useRef } from "react";
 
+type InputProps = {
+  type: string;
+  name: string;
+  value?: string;
+  shadow?: boolean;
+  placeholder?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+};
 
-const Input = () => {
+const Input = ({
+  type,
+  name,
+  value = "",
+  placeholder = "",
+  onChange = () => {},
+  shadow = false,
+}: InputProps) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
+
   return (
-    <div>Input</div>
-  )
-}
+    <input
+      ref={inputRef}
+      type={type}
+      name={name}
+      value={value}
+      placeholder={placeholder}
+      onChange={onChange}
+      className={`border p-2 w-full rounded md:rounded outline-none ${
+        shadow ? "shadow" : ""
+      } text-xs md:text-sm`}
+    />
+  );
+};
 
-export default Input
+export default Input;
