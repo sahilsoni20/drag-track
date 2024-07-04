0fd303c98d52b63d9e24340b24a5f804b8b64014
sahilsoni20 <ssahil3120@gmail.com>
2024-07-04 10:36:35 +0530

    zustand: for state management

--- src/lib/store/useContainerStore.ts
+++ src/lib/store/useContainerStore.ts
@@ -1,9 +1,32 @@
-import { DNDType } from "../types"
+import { DNDType } from "../types";
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 type ContainerStoreProps = {
-    container: DNDType[]
-}
+  containers: DNDType[];
+  setContainers: (containers: DNDType[]) => void;
+  addContainer: (title: string) => void;
+};
 
-export const useContainerStore = () => {
-
-}
+export const useContainerStore = create<ContainerStoreProps>()(
+  persist(
+    (set) => ({
+      containers: [],
+      setContainers: (containers) => set({ containers }),
+      addContainer: (title: string) =>
+        set((state) => ({
+          containers: [
+            ...state.containers,
+            {
+              id: `container-${Math.random() * 1000}`,
+              title,
+              items: [],
+            },
+          ],
+        })),
+    }),
+    {
+      name: "contaienr-storage",
+    }
+  )
+);
